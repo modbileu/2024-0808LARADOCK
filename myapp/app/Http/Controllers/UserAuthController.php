cf51ff30ae43f209e5f2f3f3a7378565ce448c12
modbileu <a0984026880@gmail.com>
2024-08-08 11:04:36 +0800

    第一步

---
+++ myapp/app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserAuthController extends Controller
+{
+    public function Login()
+    {
+        return 123456;
+    }
+}
--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=> 'user'], function () {
+    Route::group(['prefix' => 'auth'], function ()  {
+        Route::get('login', 'App\Http\Controllers\UserAuthController@Login');
+    });
+});
