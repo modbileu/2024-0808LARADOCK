201636a5bf55f5dc292ee7f26d552eb885963ef9
modbileu <a0984026880@gmail.com>
2024-08-13 19:41:50 +0800

    上傳圖片

--- myapp/app/Http/Controllers/MerchandiseController.php
+++ myapp/app/Http/Controllers/MerchandiseController.php
@@ -40,10 +40,10 @@ class MerchandiseController extends Controller
     public function MerchandiseEditProcess($merchandise_id)
     {
         // 撈取商品資料
-        $Merchandise = Merchandise::findOrFail($merchandise_id);
+        $Merchandise = Merchandise::findOrFail($merchandise_id);    //findOrFail只會抓第一個值，這串程式為ID
         // 接收輸入資料
-        $input = request()->all();
-
+        $input = request()->all();       //回傳所有
+        //dd($input) //return $input
         // if ( 錯誤判斷 ) {
         //     // 資料驗證錯誤
         //     return redirect('/merchandise/' . $Merchandise->id . '/edit')
@@ -52,27 +52,61 @@ class MerchandiseController extends Controller
         // }
 
 
-        // if (isset($input['photo'])){
-        //     // 有上傳圖片
-        //     $photo = $input['photo'];
-        //     // 檔案副檔名
-        //     $file_extension = $photo->getClientOriginalExtension();
-        //     // 產生自訂隨機檔案名稱
-        //     $file_name = uniqid() . '.' . $file_extension;
-        //     // 檔案相對路徑
-        //     $file_relative_path = 'images/merchandise/' . $file_name;s
-        //     // 檔案存放目錄為對外公開 public 目錄下的相對位置
-        //     $file_path = public_path($file_relative_path);
-        //     // 裁切圖片
-        //     $image = Image::make($photo)->fit(450, 300)->save($file_path);
-        //     // 設定圖片檔案相對位置
-        //     $input['photo'] = $file_relative_path;
-        // }
+        if (isset($input['photo'])){
+            // 有上傳圖片
+            $photo = $input['photo'];
+            // 檔案副檔名
+            $file_extension = $photo->getClientOriginalExtension();
+            // 產生自訂隨機檔案名稱
+            $file_name = uniqid() . '.' . $file_extension;
+            // 檔案相對路徑
+            $file_relative_path = 'images/merchandise/';
+            // 檔案存放目錄為對外公開 public 目錄下的相對位置
+            $file_path = public_path($file_relative_path);
+            // 裁切圖片
+            //$image = Image::make($photo)->fit(450, 300)->save($file_path);(舊版程式，現在的要改成下面那個)
+            $photo->move($file_path , $file_name);
+            // 設定圖片檔案相對位置
+            $input['photo'] = $file_relative_path . $file_name;
+        }
 
         // 商品資料更新
         $Merchandise->update($input);
 
         // 重新導向到商品編輯頁
         return redirect('/merchandise/' . $Merchandise->id . '/edit');
+    }
+
+    // 商品管理清單檢視
+    public function MerchandiseManage()
+    {
+        // 每頁資料量
+        $row_per_page = 10;
+        // 撈取商品分頁資料
+        $MerchandisePaginate = Merchandise::OrderBy('created_at', 'desc')
+            ->paginate($row_per_page);
+
+        // 設定商品圖片網址
+        foreach ($MerchandisePaginate as &$Merchandise) {
+            if (!is_null($Merchandise->photo)) {
+                // 設定商品照片網址
+                $Merchandise->photo = url($Merchandise->photo);
+            }
+        }
+
+        $binding = [
+            'title' => '商品管理',
+            'MerchandisePaginate'=> $MerchandisePaginate,
+        ];
+
+        return view('merchandise.manage', $binding);
+    }
+
+    public function MerchandiseDelete($merchandise_id)
+    {
+
+        $Merchandise = Merchandise::where('id', $merchandise_id)->delete();
+
+        return redirect(route('merchandise.manage'));
     }
 }
